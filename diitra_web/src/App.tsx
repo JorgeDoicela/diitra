b4432cfd61f24b912b1c50340372c86304ba4b39
JorgeDoicela <ismael02doicela@gmail.com>
2026-06-01 10:42:35 -0500

    Preferencia de modo claro

--- diitra_web/src/App.tsx
+++ diitra_web/src/App.tsx
@@ -123,7 +123,7 @@ function App() {
         if (typeof window !== 'undefined' && window.matchMedia) {
             return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
         }
-        return 'dark';
+        return 'light';
     });
 
     useEffect(() => {
