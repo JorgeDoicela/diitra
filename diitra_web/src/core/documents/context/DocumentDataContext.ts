0a1ad3ac378c07272667ae598512ee38281532d6
JorgeDoicela <ismael02doicela@gmail.com>
2026-06-23 10:42:14 -0500

    fix: arreglo de datos

--- diitra_web/src/core/documents/context/DocumentDataContext.ts
+++ diitra_web/src/core/documents/context/DocumentDataContext.ts
@@ -1,3 +1,4 @@
 import { createContext } from 'react';
 
 export const DocumentDataContext = createContext<any>(null);
+export const DocumentMetadataContext = createContext<{ readOnlyReason?: string }>({});
